98e0d76d0b04c03d99aa3a8b084164459915bc88
adrija-c <146987368+adrija-c@users.noreply.github.com>
2023-10-10 00:48:07 +0530

    Update Lasagna.java

--- exercises/concept/lasagna/src/main/java/Lasagna.java
+++ exercises/concept/lasagna/src/main/java/Lasagna.java
@@ -1,9 +1,23 @@
 public class Lasagna {
     // TODO: define the 'expectedMinutesInOven()' method
-
+    public static int expectedMinutesInOven() {
+        return 40;
+    }
+    
     // TODO: define the 'remainingMinutesInOven()' method
-
+    public static int remainingMinutesInOven(int remainingTime) {
+        return 40 - remainingTime;    
+    }
+    
     // TODO: define the 'preparationTimeInMinutes()' method
+    public static int preparationTimeInMinutes(int layers) {
+        return 2*layers;
+    }
 
     // TODO: define the 'totalTimeInMinutes()' method
+    public static int totalTimeInMinutes( int layers, int remainingTime) {
+        int result = 2*layers + remainingTime;
+        return result;
+    }    
+    
 }
